1ab9e0cb5d71e41a59f2b7a73b953b0885c8caa3
Renato Ivanic <IvanicRen@sbsit.com>
2025-05-23 12:48:51 +0200

    solved

--- src/main/java/codefinity/taskManager/TaskProcessorService.java
+++ src/main/java/codefinity/taskManager/TaskProcessorService.java
@@ -1,5 +1,6 @@
 package codefinity.taskManager;
 
 public interface TaskProcessorService {
+    void processTask();
     void processTasks();
 }
--- src/main/java/codefinity/taskManager/taskManagerImpl/TaskProcessorServiceImpl.java
+++ src/main/java/codefinity/taskManager/taskManagerImpl/TaskProcessorServiceImpl.java
@@ -1,5 +1,30 @@
 package codefinity.taskManager.taskManagerImpl;
 
-public class TaskProcessorServiceImpl {
+import codefinity.taskManager.Task;
+import codefinity.taskManager.TaskProcessorService;
+import codefinity.taskManager.TaskQueueService;
 
+public class TaskProcessorServiceImpl implements TaskProcessorService {
+    TaskQueueService taskQueueService;
+
+    public TaskProcessorServiceImpl(TaskQueueService taskQueueService) {
+        this.taskQueueService = taskQueueService;
+    }
+
+    @Override
+    public void processTask() {
+        if (!taskQueueService.isEmpty()) {
+            Task task_out = taskQueueService.getNextTask();
+            System.out.println("Processing Task: " + task_out);
+        }
+    }
+
+    @Override
+    public void processTasks() {
+        while (!taskQueueService.isEmpty()) {
+            Task task_out = taskQueueService.getNextTask();
+            System.out.println("Processing Task: " + task_out);
+        }
+        System.out.println("All tasks processed.");
+    }
 }
